b0b6c414a62de4c38ed8075e06dee011897c5775
togrul.agayev <togrul.agayev@mail.com>
2024-02-15 20:10:44 +0400

    js

---
+++ Toghrul/script.js
@@ -0,0 +1,43 @@
+let domInput = document.querySelector("#calc");
+let domInput2 = document.querySelector("#history");
+domInput.value = "";
+let arr = [];
+let operator = "";
+function num(key) {
+  domInput.value += key;
+}
+function del() {
+  domInput.value = "";
+  domInput2.value = "";
+  arr = [];
+}
+function equ() {
+  if (operator == "+") {
+    domInput.value = Number(domInput.value) + Number(arr[0]);
+  } else if (operator == "-") {
+    domInput.value = Number(arr[0]) - Number(domInput.value);
+  }
+}
+function sum() {
+  arr.push(Number(domInput.value));
+  if (arr.length >= 2) {
+    arr[0] = Number(arr[0]) + Number(arr[1]);
+    arr.pop();
+  }
+  domInput.value = "";
+  domInput2.value = arr;
+  operator = "+";
+}
+
+function min() {
+  arr.push(Number(domInput.value));
+  arr[0] = arr[1];
+  arr.pop();
+  if (arr.length >= 2) {
+    arr[0] = Number(arr[0]) - Number(arr[1]);
+    arr.pop();
+  }
+  domInput.value = "";
+  domInput2.value = arr;
+  operator = "-";
+}
